08199809cbc7439ca6962af77e6158a6aced7283
hazemkak <hazem.kak@intellaworld.com>
2022-04-22 19:10:05 +0200

    adding the pipeline features to the file

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -3,6 +3,8 @@
 
 #include <glm/gtx/euler_angles.hpp>
 
+#include<iostream>
+
 namespace our {
 
     // This function computes and returns a matrix that represents this transform
@@ -10,7 +12,25 @@ namespace our {
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
         //TODO: (Req 2) Write this function
-        return glm::mat4(1.0f);
+
+       
+        glm::mat4 T= glm::translate(
+            glm::mat4(1.0f),
+            position
+        );
+
+
+        glm::mat4 S=glm::scale(
+            glm::mat4(1.0f),
+            scale
+        );
+
+       glm::mat4 R= glm::yawPitchRoll( rotation[1],rotation[0],rotation[2] );
+       glm::mat4 SRT =T * R * S;
+     
+
+
+        return SRT;
     }
 
      // Deserializes the entity data and components from a json object
--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -42,6 +42,55 @@ namespace our {
         // For example, if faceCulling.enabled is true, you should call glEnable(GL_CULL_FACE), otherwise, you should call glDisable(GL_CULL_FACE)
         void setup() const {
             //TODO: (Req 3) Write this function
+
+
+            glDepthMask(depthMask);
+            glColorMask(colorMask[0],colorMask[1],colorMask[2],colorMask[3]);
+            // checking blending
+            // blending gives us the ability to render semi-transparent images with different levels of transparency
+            /*
+            Blending in OpenGL happens with the following equation:
+
+                                C_result=C_source∗F_source+C_destination∗F_destination(1)
+
+            C_source: the source color vector. This is the color output of the fragment shader.
+            C_destination: the destination color vector. This is the color vector that is currently stored in the color buffer.
+            F_source: the source factor value. Sets the impact of the alpha value on the source color.
+            F_destination: the destination factor value. Sets the impact of the alpha value on the destination color
+            */
+            if(blending.enabled){
+                glEnable(GL_BLEND); // not sure if disabled by default
+                // glBlendEquation is used to pass the equation we want to use between the source & destination
+                glBlendEquation(blending.equation);
+                // glBlendFunc we pass the alpha of the source & the destination
+                glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+                // glBendColor must be set using this func
+                glBlendColor(blending.constantColor[0],blending.constantColor[1],blending.constantColor[2],blending.constantColor[3]);
+            }else{
+                glDisable(GL_BLEND);
+            }
+
+            // checking depth_testing 
+            if(depthTesting.enabled){
+                glEnable(GL_DEPTH_TEST); // disabled by default
+                // we should clear depth buffer every frame to be updated always with the new values in our buffer
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                // setting the comparison operator of the depth test func
+                glDepthFunc(depthTesting.function); 
+            }
+
+            // checking face_cull 
+            if(faceCulling.enabled){
+                glEnable(GL_CULL_FACE); // disabled by default
+                // from now on all faces that aren't front faces are discarded which increase 50% of performance
+                // in func glCullFace we choose which face to cull'discard' Front/Back or both "default value GL_BACK"
+                glCullFace(faceCulling.culledFace);
+                // in func glFrontFace we choose that we would prefer clockwise faces than counter-clockwise faces "default value GL_CCW"
+                glFrontFace(faceCulling.frontFace);
+            }
+
+            //
+
         }
 
         // Given a json object, this function deserializes a PipelineState structure
